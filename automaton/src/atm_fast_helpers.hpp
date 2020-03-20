fcd9f3e8f035eb11073785771bfab2306bc8adcb
alter <altereg0@users.com>
2020-03-20 23:28:27 +0300

    Fast

--- automaton/src/atm_fast_helpers.hpp
+++ automaton/src/atm_fast_helpers.hpp
@@ -1,11 +1,35 @@
 #pragma once
 
+class IPin {
+public:
+    virtual void init() = 0;
+    virtual bool read() = 0;
+};
+
 template<board::DigitalPin attached_pin>
-class atm_pin_helper {
- public:
-  atm_pin_helper(const gpio::FAST_PIN<attached_pin> &pin) : pin(pin) {}
-  void set() { pin.set(); };
-  void clear() { pin.clear(); }
- private:
-  gpio::FAST_PIN<attached_pin> pin;
+class atm_pin_helper: public IPin {
+public:
+    void set() { pin.set(); };
+
+    bool read() override {
+        return pin.value();
+    }
+
+    void clear() { pin.clear(); }
+
+
+protected:
+    gpio::FAST_PIN<attached_pin> pin;
+};
+
+template<board::DigitalPin attached_pin>
+class atm_btn_helper: public atm_pin_helper<attached_pin> {
+public:
+
+    void init() override {
+        pin.set_mode(gpio::PinMode::INPUT_PULLUP);
+    }
+
+private:
+    gpio::FAST_PIN<attached_pin> pin;
 };
